ef03d7bd54255c2016e0b289cbae005624b4a2cf
Diego Gaspar <88562658+gaspartv@users.noreply.github.com>
2022-06-23 11:55:20 -0300

    Add files via upload

--- katas1.js
+++ katas1.js
@@ -1,94 +1,97 @@
 function oneThroughTwenty() {
-    
-   /* Your code goes below
-   Write a for or a while loop
-   return the result*/
-    
+  let contador = []
+    for (let i = 0; i < 20; i++) {
+      contador.push(i + 1)
+    }
+    return contador
 }
-
-//call function oneThroughTwenty
 
 function evensToTwenty() {
-    
-   /* Your code goes below
-   Write a for or a while loop
-   return the result */
-  
+  let contador = []
+    for (let i = 1; i <= 20; i++) {
+      if (i % 2 == 0) {
+        contador.push(i)
+      }
+    }
+    return contador
 }
-
-//call function evensToTwenty
 
 function oddsToTwenty() {
-    
-  /* Your code goes below
-  Write a for or a while loop
-  return the result */
-   
+  let contador = []
+    for (let i = 0; i < 20; i++) {
+      if (i % 2 != 0) {
+        contador.push(i)
+      }
+    }
+    return contador
 }
-
-//call function oddsToTwenty
 
 function multiplesOfFive() {
-    
-  /* Your code goes below
-  Write a for or a while loop
-  return the result */
-
+  let contador = []
+    for (let i = 1; i <= 100; i++) {
+      if (i % 5 == 0) {
+        contador.push(i)
+      }
+    }
+    return contador
 }
-
-//call function multiplesOfFive
 
 function squareNumbers() {
-    
- /* Your code goes below
-  Write a for or a while loop
-  return the result */
-    
+  let contador = []
+    for (let i = 1; i <= 100; i++) {
+      numero = Math.sqrt(i)
+        if (Number.isInteger(numero)) {
+          contador.push(i)
+        }
+  }
+  return contador
 }
-
-//call function squareNumbers
 
 function countingBackwards() {
-    
-  /* Your code goes below
-  Write a for or a while loop
-  return the result */
+  let contador = []
+  for (let i = 20; i > 0; i--) {
+    contador.push(i)
+  }
+  return contador
 }
-
-//call function countingBackwards
 
 function evenNumbersBackwards() {
-    
- /* Your code goes below
-  Write a for or a while loop
-  return the result */
+  let contador = []
+    for (let i = 20; i > 1; i--) {
+      if (i % 2 == 0) {
+        contador.push(i)
+      }
+    }
+    return contador
 }
-
-//call function evenNumbersBackwards
 
 function oddNumbersBackwards() {
-    
- /* Your code goes below
-  Write a for or a while loop
-  return the result */
+  let contador = []
+    for (let i = 20; i > 0; i--) {
+      if (i % 2 != 0) {
+        contador.push(i)
+      }
+    }
+    return contador
 }
-
-//call function oddNumbersBackwards
 
 function multiplesOfFiveBackwards() {
-    
- /* Your code goes below
-  Write a for or a while loop
-  return the result */
+  let contador = []
+    for (let i = 100; i > 1; i--) {
+      if (i % 5 == 0) {
+        contador.push(i)
+      }
+    }
+    return contador
 }
-
-//call function multiplesOfFiveBackwards
 
 function squareNumbersBackwards() {
-    
-   /* Your code goes below
-  Write a for or a while loop
-  return the result */
+  let contador = []
+    for (let i = 100; i > 0; i--) {
+      numero = Math.sqrt(i)
+        if (Number.isInteger(numero)) {
+          contador.push(i)
+        }
+  }
+  return contador
 }
-
-//call function squareNumbersBackwards
